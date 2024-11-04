9ea522a12c1be0c10cec70b1b352df60e41adf78
alexamaegazo <alexamaegazo5@gmail.com>
2024-11-04 19:56:29 +0800

    Initial commit with colorPref.js

---
+++ colorPref.js
@@ -0,0 +1,16 @@
+// Create an array to store colors
+let colors = ["pink", "green", "violet"];
+
+// Print the initial array
+console.log("Your favorite colors are:", colors);
+
+// Add a new color to the array
+colors.push("yellow");
+
+// Print the updated array
+console.log("Updated favorite colors:", colors);
+
+
+
+
+ 
